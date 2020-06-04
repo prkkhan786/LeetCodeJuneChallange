aa369721deaab4c7159bc78c045ca7f39d37abf3
prkkhan786 <prkkhan1@gmail.com>
2020-06-04 22:10:43 +0530

    day four

--- week1.java
+++ week1.java
@@ -27,8 +27,13 @@ class ListNode {
 public class week1 {
 
     public static void main(String[] args) {
-        Node root = new Node(10);
-        invertTree(root);
+        //Node root = new Node(10);
+        //invertTree(root);
+        char [] s = {'a','b','c'};
+        reverse(s);
+        for (char ch : s){
+            System.out.println(ch);
+        }
     }
 
     public static Node invertTree(Node root) {
@@ -49,8 +54,8 @@ public class week1 {
 
     public void deleteNode(ListNode node) {
 
-        //only node is given you need to delete that node 
-        //head is not given 
+        // only node is given you need to delete that node
+        // head is not given
 
         ListNode temp = node;
         ListNode pre = null;
@@ -60,6 +65,21 @@ public class week1 {
             temp = temp.next;
         }
         pre.next = null;
+    }
+
+    public static void reverse(char[] s)
+
+    {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
     }
 
 }
