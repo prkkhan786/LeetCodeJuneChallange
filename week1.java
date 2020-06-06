90bb3061e97f713f85a8189b43a8040496ce04a1
prkkhan786 <prkkhan1@gmail.com>
2020-06-06 21:58:23 +0530

    day 6

--- week1.java
+++ week1.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * week1
@@ -104,6 +106,16 @@ public class week1 {
             minCost += diff[i];
         }
         return minCost;
+    }
+
+    public int[][] reconstructQueue(int[][] people) {
+
+        Arrays.sort(people, (p1, p2) -> p1[0] == p2[0] ? p1[1] - p2[1] : p2[0] - p1[0]);
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < people.length; i++) {
+            list.add(people[i][1], people[i]);
+        }
+        return list.toArray(people);
     }
 
 }
